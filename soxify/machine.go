a00600f33433f88c3b0f1d5e370bdabf7cd31aa4
Patrick-Ranjit D. Madsen <pamad05+github@gmail.com>
2013-07-04 14:55:35 +0200

    lets try this again

--- soxify/machine.go
+++ soxify/machine.go
@@ -8,6 +8,7 @@ import (
 	"labix.org/v2/mgo/bson"
 	"net/http"
 	// "strconv"
+	"html/template"
 	"strings"
 	"time"
 )
@@ -352,7 +353,7 @@ func updateMachine(w http.ResponseWriter, r *http.Request, db *mgo.Database, arg
 	}
 	m.Id = m.Serial
 
-	m.Softwareoutput = strings.Replace(m.Softwareoutput, "\n", "<br/>", -1)
+	m.Softwareoutput = template.HTML(strings.Replace(m.Softwareoutput, "\n", "<br>", -1))
 
 	fmt.Printf("%v %v: Connection from %v - ip: %v\n", m.Date, m.Time, m.Hostname, m.Ip)
 
