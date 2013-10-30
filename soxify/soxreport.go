d277151c5bbaa222dda9ed1bb2f876534e16cbac
Patrick-Ranjit D. Madsen <pamad05+github@gmail.com>
2013-10-30 17:05:13 +0100

    the report is now in a tab-delimited csv-format, instead of comma-delimited

--- soxify/soxreport.go
+++ soxify/soxreport.go
@@ -40,7 +40,7 @@ func soxlist(w http.ResponseWriter, r *http.Request, db *mgo.Database, argPos in
 	fmt.Fprintf(w, "#,Hostname,Serial,Ip,OS (Build),Recon,Firewall,Date,Model,MHz,Ram,Virus (Definitions),Last Virus Scan,Sophos Issue,Outdated,SOxIssues,Comment\n")
 
 	for i, doc := range results {
-		fmt.Fprintf(w, "%v,%v,%v,%v,%v,%v,%v,%v,%v,%v,%v,%v (%v),%v,,%v,%v\n",
+		fmt.Fprintf(w, "%v\t%v\t%v\t%v\t%v\t%v\t%v\t%v\t%v\t%v\t%v\t%v (%v)\t%v,\t%v\t%v\n",
 			i+1,
 			doc.Hostname,
 			doc.Serial, // doc["hostname"], 
@@ -49,7 +49,7 @@ func soxlist(w http.ResponseWriter, r *http.Request, db *mgo.Database, argPos in
 			doc.Recon,
 			doc.Firewall, //["firewall"],
 			doc.Now,      // time.NanosecondsToUTC(int64(doc["date"].(bson.Timestamp))),
-			strings.Replace(doc.Model, ",", ".", -1),
+			doc.Model,    //strings.Replace(doc.Model, ",", ".", -1),
 			doc.Cpu,
 			doc.Memory,
 			doc.Virus_version,
